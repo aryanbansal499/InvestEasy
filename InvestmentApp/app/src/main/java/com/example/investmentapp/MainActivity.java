d7c946de8c0f8a6433a0c6108fdb748c09d868b2
Shivam Agarwal <shivam2003agarwal@gmail.com>
2019-12-01 02:18:10 +1100

    temporary

--- InvestmentApp/app/src/main/java/com/example/investmentapp/MainActivity.java
+++ InvestmentApp/app/src/main/java/com/example/investmentapp/MainActivity.java
@@ -25,47 +25,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filtered_options);
 
-        //getting the recyclerview from xml
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //initializing the productlist
-        productList = new ArrayList<>();
-
-
-        //adding some items to our list
-        productList.add(
-                new Product(
-                        1,
-                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
-                        "13.3 inch, Silver, 1.35 kg",
-                        4.3,
-                        60000,
-                        R.drawable.download));
-
-        productList.add(
-                new Product(
-                        1,
-                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
-                        "14 inch, Gray, 1.659 kg",
-                        4.3,
-                        60000,
-                        R.drawable.house));
-
-        productList.add(
-                new Product(
-                        1,
-                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
-                        "13.3 inch, Silver, 1.35 kg",
-                        4.3,
-                        60000,
-                        R.drawable.house));
-
-        //creating recyclerview adapter
-        ProductAdapter adapter = new ProductAdapter(this, productList);
-
-        //setting adapter to recyclerview
-        recyclerView.setAdapter(adapter);
+       
     }
 }
