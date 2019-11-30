bae35a4d4a226c96c7b922efce10e53f29c68312
Shivam Agarwal <shivam2003agarwal@gmail.com>
2019-11-30 19:35:19 +1100

    creation of list of investments

--- InvestmentApp/app/src/main/java/com/example/investmentapp/MainActivity.java
+++ InvestmentApp/app/src/main/java/com/example/investmentapp/MainActivity.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    String data;
+
     Classify classify = new Classify();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        classify.generate();
 
     }
 }
